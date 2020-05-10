e35b442743958fa250a534c93a64ef22cba86fcd
AdityaGattu <52188412+AdityaGattu@users.noreply.github.com>
2020-05-10 23:21:38 +0530

    Create day9_Valid Perfect Square.cpp

---
+++ day9_Valid Perfect Square.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPerfectSquare(int n) {
+        
+        for(long long i=0;i*i<=n;i++)
+        {
+            if((i*i)==n)
+            {
+                return true;
+            }
+        }
+        return false;
+        
+    }
+};
